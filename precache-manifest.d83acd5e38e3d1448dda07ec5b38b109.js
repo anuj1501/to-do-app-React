53bead5d1f5e93ed376a98c76122a28097fc33b7
anuj1501 <anuj.raghani@gmail.com>
2020-03-23 20:46:20 +0530

    Updates

---
+++ precache-manifest.d83acd5e38e3d1448dda07ec5b38b109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0550f118f4e442b9d991b05a3f89e8",
+    "url": "/to-do-app-in-React/index.html"
+  },
+  {
+    "revision": "50e071b8301ebc33655c",
+    "url": "/to-do-app-in-React/static/css/main.4ecd4967.chunk.css"
+  },
+  {
+    "revision": "4c6b5ede2d069e999e9a",
+    "url": "/to-do-app-in-React/static/js/2.bb721667.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/to-do-app-in-React/static/js/2.bb721667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e071b8301ebc33655c",
+    "url": "/to-do-app-in-React/static/js/main.cef0682a.chunk.js"
+  },
+  {
+    "revision": "0432812f6b598f3ad14b",
+    "url": "/to-do-app-in-React/static/js/runtime-main.254e0278.js"
+  }
+]);
